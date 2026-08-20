2d0f7fb591d111984dbc203279bb89845bf6d37d
João Gabriel Marques <bielpesso@gmail.com>
2026-08-20 00:01:59 -0300

    first commit

---
+++ src/main/java/space/gmarqueszx/frete_flow/FreteFlowApplication.java
@@ -0,0 +1,13 @@
+package space.gmarqueszx.frete_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/gmarqueszx/frete_flow/FreteFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package space.gmarqueszx.frete_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
